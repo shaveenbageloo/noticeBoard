5121c2969bf016e6ed6f7b44f8395045268df0ef
shaveenbagelooTHA <shaveen.bageloo@hashgraph-group.com>
2025-06-05 17:03:54 +0200

    Initial checkin

---
+++ server.js
@@ -0,0 +1,25 @@
+// server.js
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const app = express();
+const PORT = 8000;
+
+// Serve the message board
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'message.html'));
+});
+
+// Admin editor (WYSIWYG)
+app.get('/admin', (req, res) => {
+  res.sendFile(path.join(__dirname, 'editor.html'));
+});
+
+// Save edited HTML
+app.use(express.urlencoded({ extended: true }));
+app.post('/save', (req, res) => {
+  fs.writeFileSync('message.html', req.body.html);
+  res.redirect('/admin');
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
